c788c585a0f4ea32bf17fd53f50e773820933de6
Phil Hord <phord@purestorage.com>
2021-12-12 10:18:30 -0800

    Day 12 simplified

--- 12.py
+++ 12.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 from core.skel import *
+from collections import defaultdict
 
 sample = """
 start-A
@@ -12,54 +13,36 @@ A-end
 b-end
 """
 
-sample2="""
-dc-end
-HN-start
-start-kj
-dc-start
-dc-HN
-LN-dc
-HN-end
-kj-sa
-kj-HN
-kj-dc"""
-
 answer1 = None
 answer2 = None
 
 def parse(input):
-    lines = [parse_fields(x,digits+alpha) for x in input.splitlines() if x]
-    edges = {}
-    for x in lines:
-        a,b = x
-        if a not in edges:
-            edges[a] = set()
-        if b not in edges:
-            edges[b] = set()
-        edges[a].add(b)
-        edges[b].add(a)
+    edges = defaultdict(set)
+    for x in filter(lambda _:_, input.splitlines()):
+        A,B = x.split('-')
+        for a,b in [(A,B), (B,A)]:
+            if a != 'end' and b != 'start':
+                edges[a].add(b)
     return edges
 
 def traverse(edges, cave, path, visited, once):
-    if cave != 'start' and cave in visited:
-        once = True
+    once |= cave in visited
     if cave >= 'a':
         visited = visited | set([cave])
-    path = path[:]
-    path.append(cave)
 
+    path.append(cave)
     if cave == 'end':
         yield ','.join(path)
     else:
-        for dest in edges[cave] - (visited if once else set(['start'])):
+        for dest in edges[cave] - visited if once else edges[cave]:
             yield from traverse(edges, dest, path, visited, once)
+    path.pop()
 
 def part1(edges, part2=False):
     return len(list(traverse(edges, 'start', [], set(), not part2)))
 
 def part2(edges):
     return part1(edges, True)
-
 
 from aocd import data
 runAll(sample, data, parse, part1, part2, answer1, answer2)
