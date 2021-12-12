d32fcf72cecdcf4159fce88d80bb76addb101cad
Phil Hord <phord@purestorage.com>
2021-12-11 21:30:17 -0800

    Day 11 and 12

--- 11.py
+++ 11.py
@@ -3,16 +3,62 @@
 from core.skel import *
 
 sample = """
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
 """
-answer1 = None
-answer2 = None
+answer1 = 1656
+answer2 = 195
+
+flashes = 0
 
 def parse(input):
-    lines = [parse_fields(x,digits+alpha) for x in input.splitlines() if x]
+    lines = [[int(y) for y in x] for x in input.splitlines() if x]
     return lines
 
+def flash(grid, x, y):
+    for X in range(max([0, x-1]),min([x+2, 10])):
+        for Y in range(max([0, y-1]),min([y+2, 10])):
+            inc(grid, X, Y)
+
+def inc(grid, x, y):
+    grid[x][y] += 1
+    if grid[x][y] == 10:
+        flash(grid, x, y)
+
+def rollover(grid):
+    global flashes
+    for y in range(10):
+        for x in range(10):
+            if grid[x][y] > 9:
+                grid[x][y] = 0
+                flashes += 1
+
+def step(grid):
+    for y in range(10):
+        for x in range(10):
+            inc(grid, x, y)
+    rollover(grid)
+
 def part1(input, part2=False):
-    return None
+    global flashes
+    flashes = 0
+
+    prev = flashes
+    for x in range(10000 if part2 else 100):
+        step(input)
+        if part2 and flashes - prev == 100:
+            return x+1
+        prev = flashes
+        # print(x, flashes)
+    return flashes
 
 
 def part2(input):
--- 12.py
+++ 12.py
@@ -3,20 +3,69 @@
 from core.skel import *
 
 sample = """
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
 """
+
+sample2="""
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
+
 answer1 = None
 answer2 = None
 
 def parse(input):
     lines = [parse_fields(x,digits+alpha) for x in input.splitlines() if x]
-    return lines
+    edges = {}
+    for x in lines:
+        a,b = x
+        if a not in edges:
+            edges[a] = set()
+        if b not in edges:
+            edges[b] = set()
+        edges[a].add(b)
+        edges[b].add(a)
+    return edges
 
-def part1(input, part2=False):
-    return None
+count = 0
+def traverse(edges, cave, path, visited, once):
+    global count
+    if cave != 'start' and cave in visited:
+        once = True
+    if cave >= 'a':
+        visited = visited | set([cave])
+    path = [x for x in path]
+    path.append(cave)
+    if cave == 'end':
+        count += 1
+        # print(f"{count}. {once} {','.join(path)}")
+        return
+
+    for dest in edges[cave] - (visited if once else set(['start'])):
+        traverse(edges, dest, path, visited, once)
+
+def part1(edges, part2=False):
+    global count
+    count = 0
+    traverse(edges, 'start', [], set(), not part2)
+    return count
 
 
-def part2(input):
-    return part1(input, True)
+def part2(edges):
+    return part1(edges, True)
 
 
 from aocd import data
