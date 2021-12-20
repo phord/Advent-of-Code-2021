ee73c72100607b7596077e06e1f9c4e32d926021
Phil Hord <phord@purestorage.com>
2021-12-20 12:17:04 -0800

    Day 20 - less hacky

--- 20.py
+++ 20.py
@@ -10,113 +10,84 @@ sample = """
 ##..#
 ..#..
 ..###"""
-answer1 = None
-answer2 = None
-
-
-# class Grid:
+answer1 = 35
+answer2 = 3351
 
 def parse(input):
     lines = [x for x in input.splitlines() if x]
     return lines
 
-def to_grid(image):
-    grid = set()
-    for row in range(len(image)):
-        for col in range(len(image[row])):
-            if image[row][col] == '#':
-                grid.add((row, col))
-    return grid
+class Image:
 
-def bit(image, row, col):
-    default, size, grid = image
-    tl, br = size
-    x0,y0 = tl
-    x1,y1 = br
+    def __init__(self, input):
+        self.from_input(input)
 
-    return (row,col) in grid or (default and (col < x0 or col > x1 or row < y0 or row > y1))
+    def from_input(self, lines):
+        self.decoder = [x=='#' for x in lines[0]]
+        self.grid = set()
+        self.default_lit = False        # The world outside our mapped cells is dark
 
-def binary(image, row, col):
-    default, size, grid = image
-    tl, br = size
-    x0,y0 = tl
-    x1,y1 = br
+        for row in range(1,len(lines)):
+            for col in range(len(lines[row])):
+                if lines[row][col] == '#':
+                    self.grid.add((row-1, col))
+        self.x0, self.y0, self.x1, self.y1 = (0, 0, row-1, col)
 
-    str = []
-    for r in range(row,row+3):
-        str.extend([(r, c) for c in range(col, col+3)])
-    b = 0
-    for cell in str:
-        b *= 2
-        if bit(image, *cell):
-            b += 1
-    return b
+    def read_pixel(self, row, col):
+        if col < self.x0 or col > self.x1 or row < self.y0 or row > self.y1:
+            return self.default_lit
+        else:
+            return (row,col) in self.grid
 
-def output(image, decoder, row, col):
-    return decoder[binary(image, row, col)] == '#'
+    def binary(self, row, col):
+        map = [(r, c) for r in range(row,row+3) for c in range(col, col+3) ]
+        b = 0
+        for cell in map:
+            b *= 2
+            if self.read_pixel(*cell):
+                b += 1
+        return b
 
-def step(image, decoder):
-    default, size, _ = image
-    tl, br = size
-    x0,y0 = tl
-    x1,y1 = br
+    def step(self):
+        next = set()
+        for row in range(self.y0-2, self.y1+2):
+            for col in range(self.x0-2, self.x1+2):
+                if self.decoder[self.binary(row, col)]:
+                    next.add((row, col))
 
-    grid = set()
-    for row in range(y0-2, y1+30):
-        for col in range(x0-2, x1+3):
-            if output(image, decoder, row, col):
-                grid.add((row, col))
+        self.grid = next
+        self.default_lit = self.decoder[511 if self.default_lit else 0]
+        self.measure()
 
-    default, _, _ = image
-    if default:
-        default = decoder[511] == '#'
-    else:
-        default = decoder[0] == '#'
-    return (default, measure(grid), grid)
+    def measure(self):
+        exes = [_[1] for _ in self.grid]
+        whys = [_[0] for _ in self.grid]
+        self.x0 = min(exes)
+        self.y0 = min(whys)
+        self.x1 = max(exes)
+        self.y1 = max(whys)
 
-def measure(grid):
-    x0,y0, x1,y1 = (0,0,0,0)
-    # print(len(grid))
-    for c in grid:
-        y,x = c
-        x0 = min(x, x0)
-        y0 = min(y, y0)
-        x1 = max(x, x1)
-        y1 = max(y, y1)
-    return ((x0,y0), (x1,y1))
-
-def dump(image):
-    default, size, grid = image
-    tl, br = size
-    x0,y0 = tl
-    x1,y1 = br
-
-    for row in range(y0-3, y1+1+3):
-        str = ['#' if bit(image, row, col) else '.' for col in range(x0-3, x1+1+3)]
-        print(''.join(str))
-    print("---")
+    def dump(self):
+        print(f"{self.x0}, {self.y0} -- {self.x1}, {self.y1}")
+        margin = 3
+        for row in range(self.y0-margin, self.y1+1+margin):
+            str = ['#' if self.read_pixel(row, col) else '.' for col in range(self.x0-margin, self.x1+1+margin)]
+            print(f"{row}.", ''.join(str))
+        print("---")
 
 
 def part1(input, part2=False):
-    decoder = input[0]
-    image = input[1:]
-
-    grid = to_grid(image)
-
-    grid = (False, measure(grid), grid)
+    grid = Image(input)
 
     for x in range(50 if part2 else 2):
-        # dump(grid)
-        grid = step(grid, decoder)
-    dump(grid)
+        grid.step()
+    # grid.dump()
 
-
-    return len(grid[2])
+    return len(grid.grid)
 
 
 def part2(input):
     return part1(input, True)
-
 
 from aocd import data
 runAll(sample, data, parse, part1, part2, answer1, answer2)
