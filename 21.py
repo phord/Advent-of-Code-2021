77219181ceb3c2cfe7c023de0c17387352184730
Phil Hord <phord@purestorage.com>
2021-12-22 10:47:17 -0800

    Day 21 - Brute force with memoization

--- 21.py
+++ 21.py
@@ -1,83 +1,27 @@
 #!/bin/python3
 
-from collections import defaultdict
-from core.skel import *
+from functools import lru_cache
 
-sample = ""
+# Each time we call play, it is player1's turn.  (We swap them on each call.)
+@lru_cache(maxsize=None)
+def play(pos1, pos2, score1, score2):
+    # Player2 just moved.  See if he won.
+    if score2 >= 21:
+        return [0, 1]
 
-answer1 = None
-answer2 = None
+    wins1 = 0
+    wins2 = 0
+    for die1 in range(1,4):
+        for die2 in range(1,4):
+            for die3 in range(1,4):
+                newpos = (pos1 + die1 + die2 + die3 - 1) % 10 + 1
+                w1, w2 = play(pos2, newpos, score2, score1 + newpos)
+                wins1 += w1
+                wins2 += w2
 
-def parse(input):
-    return (4,2)
+    # swap the wins when we return
+    return [wins2, wins1]
 
-def part1(input):
-
-    pos = [4,8]  # sample
-    pos = [4,2]  # my input
-    score = [0,0]
-    die = 0
-    turn = 0
-    rolls = 0
-    while max(score) < 1000:
-        for r in range(3):
-            die = die % 100 + 1
-            pos[turn] += die
-            rolls += 1
-        pos[turn] = (pos[turn] - 1) % 10 + 1
-        score[turn] += pos[turn]
-        print(f"Player {turn+1} rolls {die-2}+{die-1}+{die} and moves to space {pos[turn]} for a total score of {score[turn]}")
-        turn = (turn + 1) % 2
-
-    return min(score) * rolls
-
-def part2(input):
-    start_pos = [4,8]  # sample
-    start_pos = [4,2]  # my input
-
-    turn = 0
-    universes = defaultdict(int)
-
-    universes[(start_pos[0], start_pos[1], 0, 0)] = 1
-
-    # Count all possible outcomes of rolling 3 3-sided dice
-    possible_rolls = defaultdict(int)
-    for d1 in range(1,4):
-        for d2 in range(1,4):
-            for d3 in range(1,4):
-                possible_rolls[d1+d2+d3] += 1
-
-    # Play games in all universes at once
-    in_progress = True
-    while in_progress:
-        in_progress = False
-        next = defaultdict(int)
-        for key in universes:
-            p1, p2, s1, s2 = key
-            if max([s1,s2]) < 21:
-                in_progress = True
-                for roll in possible_rolls:
-                    p1, p2, s1, s2 = key
-                    if turn == 0:
-                        p1 = (p1 - 1 + roll) % 10 + 1
-                        s1 += p1
-                    else:
-                        p2 = (p2 - 1 + roll) % 10 + 1
-                        s2 += p2
-                    next[(p1, p2, s1, s2)] += possible_rolls[roll] * universes[key]
-            else:
-                if universes[key]:
-                    next[key] += universes[key]
-        print(f"{turn+1}. {len(next)} {sum(next.values())}")
-        universes = next
-        turn = (turn + 1) % 2
-
-    win1 = sum([count for key, count in universes.items() if key[2] >= 21])
-    win2 = sum([count for key, count in universes.items() if key[3] >= 21])
-    print(win1, " ", win2)
-
-    return max([win1, win2])
-
-
-from aocd import data
-runAll(sample, data, parse, part1, part2, None, None)
+start_pos = [4,2]  # My input
+wins = play(*start_pos, 0, 0)
+print(f"Answer={max(wins)}")
