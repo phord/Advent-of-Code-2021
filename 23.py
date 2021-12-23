66b58cb51b61cba1cd08671e788c0a8b3f1db8a3
Phil Hord <phord@purestorage.com>
2021-12-23 04:36:59 -0800

    wip: Day 23 part2  going to bed

--- 23.py
+++ 23.py
@@ -26,84 +26,49 @@ answer2 = None
 #   4 5 6 7
 #   0 1 2 3
 #
-room = [(0,4), (1,5), (2,6), (3,7)]
+room = [tuple(range(x-8,x+8,4)) for x in range(4)]
 # room = tuple(range(8))
 parking = (8,9,11,13,15,17,18)
-# paths = {
-#     0: (4),
-#     1: (5),
-#     2: (6),
-#     3: (7),
-#     4: (0,10),
-#     5: (1,12),
-#     6: (2,14),
-#     7: (3,16),
-#     9: (8,10),
-#     10: (4,9,11),
-#     11: (10,12),
-#     12: (5,11,13),
-#     13: (14,12),
-#     14: (6,13,15),
-#     15: (14,16),
-#     16: (7,15,17),
-#     17: (16,18),
-#     18: (17),
-# }
-
-entrance = { ##       Hallway to room transition
-    0:  10,
-    1:  12,
-    2:  14,
-    3:  16,
-    4:  10,
-    5:  12,
-    6:  14,
-    7:  16
-}
-
-paths = { ##       Distance to parking      HEAD
-    0:  (parking, (4,  3, 3, 5, 7, 9, 10),   10 ),
-    1:  (parking, (6,  5, 3, 3, 5, 7,  8),   12 ),
-    2:  (parking, (8,  7, 5, 3, 3, 5,  6),   14 ),
-    3:  (parking, (10, 9, 7, 5, 3, 3,  4),   16 ),
-    4:  (parking, (3,  2, 2, 4, 6, 8,  9),   10 ),
-    5:  (parking, (5,  4, 2, 2, 4, 6,  7),   12 ),
-    6:  (parking, (7,  6, 4, 2, 2, 4,  5),   14 ),
-    7:  (parking, (9,  8, 6, 4, 2, 2,  3),   16 ),
-    #              Distance to rooms 0..7
-    8:  (room,    (4, 6, 8, 10, 3, 5, 7, 9)),
-    9:  (room,    (3, 5, 7,  9, 2, 4, 6, 8)),
-    # 10: (room,    ()),
-    11: (room,    (3, 3, 5,  7, 2, 2, 4, 6)),
-    # 12: (room,    ()),
-    13: (room,    (5, 3, 3,  5, 4, 2, 2, 5)),
-    # 14: (room,    ()),
-    15: (room,    (7, 5, 3, 3,  6, 4, 2, 2)),
-    # 16: (room,    ()),
-    17: (room,    (9, 7, 5, 3,  8, 6, 4, 2)),
-    18: (room,   (10, 8, 6, 4,  9, 7, 5, 3)),
-}
 
 #############
 #...........#
 ###B#C#B#D###
+  #D#C#B#A#  <-- Part 2
+  #D#B#A#C#  <-- Part 2
 # #A#D#C#A#
 # #########
-#         A, A, B, B, C, C, D, D
-sample_data = [0, 3, 4, 6, 5, 2, 1, 7]
+#              A, A, A, A,  B,   B, B,  B, C, C, C,  C, D,  D, D, D
+sample_data = [0, 3, 6, 11, 12, 14, 5, 10, 2, 7, 9, 13, 1, 15, 4, 8]
 
 #############
 #...........#
 ###B#B#C#D###
+  #D#C#B#A#  <-- Part 2
+  #D#B#A#C#  <-- Part 2
 #  D#A#A#C#
 #  ########
-#         A, A, B, B, C, C, D, D
-real_data  = [1, 2, 4, 5, 3, 6, 0, 7]
-energy = [10**(x//2) for x in range(8)]
+#              A, A, A, A,  B,   B, B,  B, C, C, C,  C, D,  D, D, D
+real_data   = [1, 2, 6, 11, 12, 13, 5, 10, 3, 14, 7, 9, 0, 15, 4, 8]
+
+part2 = [2, 1, 3, 0, 7, 6, 5, 4]
+
+sample_data = [x-8 for x in sample_data]
+print(sorted(sample_data))
+print(sorted(list(set(sample_data))))
+assert len(sample_data) == 16
+assert len(set(sample_data)) == 16
+
+real_data = [x-8 for x in real_data]
+assert len(real_data) == 16
+assert len(set(real_data)) == 16
+
+
+
+energy = [10**(x//4) for x in range(16)]
 
 def parse(input):
-    # if input == 'xxx':
-    #     return sample_data
+    if input == 'xxx':
+        return sample_data
     return real_data
 
 # There are 7 parking spots in the hallway where a pod can rest.
@@ -126,8 +91,9 @@ def clear_to_go_home(data, dest, head):
 
 def can_go_home(data, pod):
     pos = data[pod]
-    pair_pos = data[pod ^ 1]        # position of our partner
-    goal = room[pod//2]
+    r = pod - (pod%4)
+    pair_pos = [data[p] for p in range(r,r+4) if p != pod]        # position of our partners
+    goal = room[pod//4]
 
     # Pod is in a room
     if pos < 8:
@@ -138,105 +104,115 @@ def can_go_home(data, pod):
     if not dests:
         # We can't go anywhere yet
         return False
-    elif len(dests) < 2 and pair_pos != goal[0]:
+    elif len(dests) + len(set(pair_pos).intersection(goal)) != 4:
         # Stranger in our room. Can't go home yet
         return False
     else:
         dest = min(dests)
-        head = paths[dest][2]
+        head = 10 + pod//4*2  # 10, 12, 14, 16
         if all_clear(data, pos, head):
             return True
 
 # Return all the valid moves for given pod
 def moves(data, pod):
     pos = data[pod]
-    pair_pos = data[pod ^ 1]        # position of our partner
-    goal = room[pod//2]
+    r = pod - (pod%4)
+    pair_pos = [data[p] for p in range(r,r+4) if p != pod]        # position of our partners
+    goal = room[pod//4]
 
     # Pod is in a room
     if pos < 8:
-        if pos in goal and (pos < 4 or pair_pos in goal):
-            # We are done
-            # print(f"{pos} is home")
-            return
-        elif pos < 4 and pos + 4 in data:
+        # Who is below me?
+
+        if pos in goal:
+            below_me = frozenset(range(pos%4-8,pos,4))
+            if len([x for x in pair_pos if x in below_me]) == len(below_me):
+                # We are done
+                print(f"{pos} is home")
+                return
+        elif pos < 4 and any([x in data for x in range(pos + 4, 8, 4)]):
             # Someone is blocking our way
-            # print(f"{pos} can't leave the room")
+            print(f"{pos} can't leave the room")
             return
         else:
             # Move to parking place in hallway
-            targets, distance, head = paths[pos]
-            for dest, dist in [(dest, dist) for dest, dist in zip(targets, distance) if all_clear(data, dest, head)]:
+            head = 10 + pod//4*2  # 10, 12, 14, 16
+            for dest in [d for d in parking if all_clear(data, d, head)]:
+                dist = 4-(pos + 8)//4 + abs(dest-head)
                 yield (dest, dist)
 
     # Pod is in the hallway already
     else:
-        dests = set(goal) - set(data)
-        if not dests:
-            # print(f"{pos} room is full")
-            # We can't go anywhere yet
-            return
-        elif len(dests) < 2 and pair_pos != goal[0]:
+        dests = set(goal) - (set(data) - set(pair_pos))
+        if len(dests) < 4:
             # Stranger in our room. Can't go home yet
-            # print(f"{pos} room is occupied")
+            print(f"{pos} room is occupied")
             return
         else:
-            dest = min(dests)
-            distance = paths[pos][1]
-            head = paths[dest][2]
+            dest = min(dests - set(pair_pos))
+            head = 10 + pod//4*2  # 10, 12, 14, 16
             if clear_to_go_home(data, pos, head):
-                dist = distance[dest]
+                dist = 4-(dest + 8)//4 + abs(pos-head)
                 yield (dest, dist)
             else:
-                # print(f"{pos} path is blocked {data} {pos} {head}")
+                print(f"{pos} path is blocked {data} {pos} {head}")
                 return
 
 
 def solved(data):
-    return all([x in room[pod//2] for pod, x in enumerate(data)])
+    return all([x in room[pod//4] for pod, x in enumerate(data)])
 
 def dump(data):
     d = set(data)
-    revd = ['.']*19
+    revd = ['.']*27
     for pod, pos in enumerate(data):
-        revd[pos] = chr(65+pod//2)
-    hallway = ''.join(revd[8:])
-    rm1 = '#'.join(revd[4:8])
+        revd[pos+8] = chr(65+pod//4)
+    hallway = ''.join(revd[16:])
+    rm1 = '#'.join(revd[12:16])
+    rma = '#'.join(revd[8:12])
+    rmb = '#'.join(revd[4:8])
     rm2 = '#'.join(revd[0:4])
     print(f"""#############
 #{hallway}#
 ###{rm1}###
+  #{rma}#
+  #{rmb}#
   #{rm2}#
   #########""")
 
 @lru_cache(maxsize=None)
 def estimate_energy_needed(data):
     E = 0
-    for pod in range(8):
+    for pod in range(16):
         pos = data[pod]
-        pair_pos = data[pod ^ 1]        # position of our partner
-        goal = room[pod//2]
+        r = pod - (pod%4)
+        pair_pos = [data[p] for p in range(r,r+4) if p != pod]        # position of our partners
+        goal = room[pod//4]
+        head = 10 + pod//4*2  # 10, 12, 14, 16
+
+        # TODO: Detect deadlock?  We want to move to park, but all paths are blocked.
+        #       Or B and C are parked but need to swap places (in a way that can never happen)
 
         # Pod is in a room
         if pos < 8:
-            if pos in goal and (pos < 4 or pair_pos in goal):
-                # We are done
-                # print(f"{pos} is home")
-                continue
-            # elif pos < 4 and pos + 4 in data:
+            if pos in goal:
+                below_me = frozenset(range(pos%4-8,pos,4))
+                if len([x for x in pair_pos if x in below_me]) == len(below_me):
+                    # We are done
+                    # print(f"{pos} is home")
+                    continue
+            # elif pos < 4 and any([x in data for x in range(pos + 4, 8, 4)]):
             #     # Someone is blocking our way
-            #     # print(f"{pos} can't leave the room")
+            #     print(f"{pos} can't leave the room")
             #     return
             else:
                 # Move to parking place in hallway
-                targets, distance, head = paths[pos]
-                dist, pos = min([(dist, dest) for dest, dist in zip(targets, distance)])
+                dist = 4-(pos + 8)//4 + 1  # Minimum distance to park
                 E += dist * energy[pod]
+                pos = head + 1
 
         # Pod is now in the hallway (hypothetically)
-        dest = max(goal)
-        distance = paths[pos][1]
-        dist = distance[dest]
+        dist = abs(pos-head) + 1   # Minimum distance.  TODO: Add 1 for every pair_pos in parking.
         E += dist * energy[pod]
 
     # E should already be a very liberal estimate, on the low side.  But we're missing opportunities somehow.
@@ -289,10 +265,12 @@ def solve(game):
 
 def part1(data, part2=False):
     global solutions
-    solutions = [15113]
+    solutions = []
+    # dump(sample_data)
+    # dump(real_data)
     solve(Game(data))
     print(len(solutions), min(solutions) if solutions else 0)
-    return min(solutions)
+    return min(solutions) if solutions else 0
 
 
 def part2(input):
