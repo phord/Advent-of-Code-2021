3f0b62054e9630d29e15ae0b859bb22a885c501a
Phil Hord <phord@purestorage.com>
2021-12-22 17:50:32 -0800

    Day 22 - cleaned up

--- 22.py
+++ 22.py
@@ -1,142 +1,37 @@
 #!/bin/python3
 
 from core.skel import *
-from functools import lru_cache
-
-sample = """
-on x=10..12,y=10..12,z=10..12
-on x=11..11,y=11..11,z=11..11
-    on x=9..9,y=9..9,z=9..9
-"""
-
-sample = """
-    on x=10..12,y=10..12,z=10..12
-    on x=11..13,y=11..13,z=11..13
-    off x=9..11,y=9..11,z=9..11
-    on x=10..10,y=10..10,z=10..10
-"""
-
-sample="""
-    on x=-20..26,y=-36..17,z=-47..7
-    on x=-20..33,y=-21..23,z=-26..28
-    on x=-22..28,y=-29..23,z=-38..16
-    on x=-46..7,y=-6..46,z=-50..-1
-    on x=-49..1,y=-3..46,z=-24..28
-    on x=2..47,y=-22..22,z=-23..27
-    on x=-27..23,y=-28..26,z=-21..29
-    on x=-39..5,y=-6..47,z=-3..44
-    on x=-30..21,y=-8..43,z=-13..34
-    on x=-22..26,y=-27..20,z=-29..19
-    off x=-48..-32,y=26..41,z=-47..-37
-    on x=-12..35,y=6..50,z=-50..-2
-    off x=-48..-32,y=-32..-16,z=-15..-5
-    on x=-18..26,y=-33..15,z=-7..46
-    off x=-40..-22,y=-38..-28,z=23..41
-    on x=-16..35,y=-41..10,z=-47..6
-    off x=-32..-23,y=11..30,z=-14..3
-    on x=-49..-5,y=-3..45,z=-29..18
-    off x=18..30,y=-20..-8,z=-3..13
-    on x=-41..9,y=-7..43,z=-33..15
-    on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-    on x=967..23432,y=45373..81175,z=27513..53682"""
-
-sample="""
-    on x=-5..47,y=-31..22,z=-19..33
-    on x=-44..5,y=-27..21,z=-14..35
-    on x=-49..-1,y=-11..42,z=-10..38
-    on x=-20..34,y=-40..6,z=-44..1
-    off x=26..39,y=40..50,z=-2..11
-    on x=-41..5,y=-41..6,z=-36..8
-    off x=-43..-33,y=-45..-28,z=7..25
-    on x=-33..15,y=-32..19,z=-34..11
-    off x=35..47,y=-46..-34,z=-11..5
-    on x=-14..36,y=-6..44,z=-16..29
-    on x=-57795..-6158,y=29564..72030,z=20435..90618
-    on x=36731..105352,y=-21140..28532,z=16094..90401
-    on x=30999..107136,y=-53464..15513,z=8553..71215
-    on x=13528..83982,y=-99403..-27377,z=-24141..23996
-    on x=-72682..-12347,y=18159..111354,z=7391..80950
-    on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
-    on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
-    on x=-52752..22273,y=-49450..9096,z=54442..119054
-    on x=-29982..40483,y=-108474..-28371,z=-24328..38471
-    on x=-4958..62750,y=40422..118853,z=-7672..65583
-    on x=55694..108686,y=-43367..46958,z=-26781..48729
-    on x=-98497..-18186,y=-63569..3412,z=1232..88485
-    on x=-726..56291,y=-62629..13224,z=18033..85226
-    on x=-110886..-34664,y=-81338..-8658,z=8914..63723
-    on x=-55829..24974,y=-16897..54165,z=-121762..-28058
-    on x=-65152..-11147,y=22489..91432,z=-58782..1780
-    on x=-120100..-32970,y=-46592..27473,z=-11695..61039
-    on x=-18631..37533,y=-124565..-50804,z=-35667..28308
-    on x=-57817..18248,y=49321..117703,z=5745..55881
-    on x=14781..98692,y=-1341..70827,z=15753..70151
-    on x=-34419..55919,y=-19626..40991,z=39015..114138
-    on x=-60785..11593,y=-56135..2999,z=-95368..-26915
-    on x=-32178..58085,y=17647..101866,z=-91405..-8878
-    on x=-53655..12091,y=50097..105568,z=-75335..-4862
-    on x=-111166..-40997,y=-71714..2688,z=5609..50954
-    on x=-16602..70118,y=-98693..-44401,z=5197..76897
-    on x=16383..101554,y=4615..83635,z=-44907..18747
-    off x=-95822..-15171,y=-19987..48940,z=10804..104439
-    on x=-89813..-14614,y=16069..88491,z=-3297..45228
-    on x=41075..99376,y=-20427..49978,z=-52012..13762
-    on x=-21330..50085,y=-17944..62733,z=-112280..-30197
-    on x=-16478..35915,y=36008..118594,z=-7885..47086
-    off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
-    off x=2032..69770,y=-71013..4824,z=7471..94418
-    on x=43670..120875,y=-42068..12382,z=-24787..38892
-    off x=37514..111226,y=-45862..25743,z=-16714..54663
-    off x=25699..97951,y=-30668..59918,z=-15349..69697
-    off x=-44271..17935,y=-9516..60759,z=49131..112598
-    on x=-61695..-5813,y=40978..94975,z=8655..80240
-    off x=-101086..-9439,y=-7088..67543,z=33935..83858
-    off x=18020..114017,y=-48931..32606,z=21474..89843
-    off x=-77139..10506,y=-89994..-18797,z=-80..59318
-    off x=8476..79288,y=-75520..11602,z=-96624..-24783
-    on x=-47488..-1262,y=24338..100707,z=16292..72967
-    off x=-84341..13987,y=2429..92914,z=-90671..-1318
-    off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
-    off x=-27365..46395,y=31009..98017,z=15428..76570
-    off x=-70369..-16548,y=22648..78696,z=-1892..86821
-    on x=-53470..21291,y=-120233..-33476,z=-44150..38147
-    off x=-93533..-4276,y=-16170..68771,z=-104985..-24507
-"""
-
-answer1 = 474140
-answer2 = 2758514936282235
 
 def parse(input):
     lines = [parse_fields(x,digits+alpha+'-') for x in input.splitlines() if x]
-    return lines
+
+    instructions = []
+    for row in lines:
+        if not row:
+            continue
+        cmd,_,x0,x1,_,y0,y1,_,z0,z1 = row
+        x0,x1 = sorted([int(x0),int(x1)])
+        y0,y1 = sorted([int(y0),int(y1)])
+        z0,z1 = sorted([int(z0),int(z1)])
+
+        cube = (x0,x1,y0,y1,z0,z1)
+
+        instructions.append((cmd == 'on', cube))
+
+    return instructions
 
 def part1(input, part2=False):
     cells = set()
 
     for row in input:
-        cmd,_,x0,x1,_,y0,y1,_,z0,z1 = row
-        x0=int(x0)
-        y0=int(y0)
-        z0=int(z0)
-        x1=int(x1)
-        y1=int(y1)
-        z1=int(z1)
-        on = cmd == 'on'
-        x0,x1 = sorted([x0,x1])
-        y0,y1 = sorted([y0,y1])
-        z0,z1 = sorted([z0,z1])
+        on, cube = row
+
+        x0,x1,y0,y1,z0,z1 = cube
 
         if x1 < -50 or x0 > 50: continue
         if y1 < -50 or y0 > 50: continue
         if z1 < -50 or z0 > 50: continue
-        x0 = max(x0,-50)
-        y0 = max(y0,-50)
-        z0 = max(z0,-50)
-        x1 = min(x1,50)
-        y1 = min(y1,50)
-        z1 = min(z1,50)
 
-        # print([cmd,x0,x1,y0,y1,z0,z1])
         for x in range(x0,x1+1):
             for y in range(y0,y1+1):
                 for z in range(z0,z1+1):
@@ -150,29 +45,17 @@ def part1(input, part2=False):
     return len(cells)
 
 
+# Return the endpoints where two 1-dimensional line segments overlap, or [] if they don't
+#        a0         a1
+# --------===========
+#             *     *
+# ------------===========
+#            b0         b1
 def overlap_segment(a0,a1,b0,b1):
     if a1 < b0 or a0 > b1:
         return []
+    # given four endpoints, the overlap points of the two segments are the middle two points when sorted
     return sorted([a0,a1,b0,b1])[1:3]
-
-
-def overlap(cube1, cube2):
-    xsplit = overlap_segment(*cube1[0:2], *cube2[0:2])
-    ysplit = overlap_segment(*cube1[2:4], *cube2[2:4])
-    zsplit = overlap_segment(*cube1[4:6], *cube2[4:6])
-
-    if xsplit and ysplit and zsplit:
-        print("Cubes overlap", cube1, cube2, xsplit, ysplit, zsplit)
-        return True
-
-    return False
-
-def inside(cube1, cube2):
-    xa0,xa1,ya0,ya1,za0,za1 = cube1
-    xb0,xb1,yb0,yb1,zb0,zb1 = cube2
-
-    return xa0 >= xb0 and xa1 <= xb1 and ya0 >= yb0 and ya1 <= yb1 and za0 >= zb0 and za1 <= zb1
-
 
 def cube_size(cube):
     x0,x1,y0,y1,z0,z1 = cube
@@ -181,10 +64,10 @@ def cube_size(cube):
 # Split cube at cx; leave cx in 2nd cube
 def cleave_x(cube, cx):
     x0,x1,y0,y1,z0,z1 = cube
-    cubes = set()
     if x1 < cx or x0 > cx:
         return set([cube])
 
+    cubes = set()
     if x0 < cx:
         cubes.add((x0,cx-1,y0,y1,z0,z1))
     cubes.add((cx,x1,y0,y1,z0,z1))
@@ -193,10 +76,10 @@ def cleave_x(cube, cx):
 # Split cube at cy; leave cy in 2nd cube
 def cleave_y(cube, cy):
     x0,x1,y0,y1,z0,z1 = cube
-    cubes = set()
     if y1 < cy or y0 > cy:
         return set([cube])
 
+    cubes = set()
     if y0 < cy:
         cubes.add((x0,x1,y0,cy-1,z0,z1))
     cubes.add((x0,x1,cy,y1,z0,z1))
@@ -205,39 +88,35 @@ def cleave_y(cube, cy):
 # Split cube at cz; leave cz in 2nd cube
 def cleave_z(cube, cz):
     x0,x1,y0,y1,z0,z1 = cube
-    cubes = set()
     if z1 < cz or z0 > cz:
         return set([cube])
 
+    cubes = set()
     if z0 < cz:
         cubes.add((x0,x1,y0,y1,z0,cz-1))
     cubes.add((x0,x1,y0,y1,cz,z1))
     return cubes
 
 # Split cube at x, y and z
-def cleave(cube, x, y, z):
-    # print(f"Cleave {cube}  on x:{x} y:{y} z:{z}")
-    cubes = cleave_x(cube, x)
-    # print(f"  on x:{x}  {cubes}")
-    c2 = set()
-    for c in cubes:
-        c2 |= cleave_y(c, y)
-    # print(f"  on y:{y}  {c2}")
-    c3 = set()
-    for c in c2:
-        c3 |= cleave_z(c, z)
-    # print(f"  on z:{z}  {c3}")
-    return c3
+def cleave_1point(cube, x, y, z):
+    x_cleaved = cleave_x(cube, x)
+    xy_cleaved = set()
+    for c in x_cleaved:
+        xy_cleaved |= cleave_y(c, y)
+    xyz_cleaved = set()
+    for c in xy_cleaved:
+        xyz_cleaved |= cleave_z(c, z)
+    return xyz_cleaved
 
-# Find the size of the occupied universe
-def universe(cubes):
-    axes = []
-    for x in range(6):
-        axes.append([c[1][x] for c in cubes])
-    size = (min(axes[0]),max(axes[0]),min(axes[1]),max(axes[1]),min(axes[2]),max(axes[2]))
-    return size
+def cleave_2points(cube, p1, p2):
+    temp = cleave_1point(cube, *p1)
+    ans = set()
+    # Cleave all resulting subcubes at point 2
+    for c in temp:
+        ans |= cleave_1point(c, *p2)
+    return ans
 
-# returns a set of cubes that define the sliced-up cubes that overlap and don't overlap
+# returns a pair of sets of cubes that define the sliced-up cubes from cube1 and from cube2
 def intersect(cube1, cube2):
     xsplit = overlap_segment(*cube1[0:2], *cube2[0:2])
     if xsplit:
@@ -245,15 +124,12 @@ def intersect(cube1, cube2):
         if ysplit:
             zsplit = overlap_segment(*cube1[4:6], *cube2[4:6])
             if zsplit:
-                tcubes1 = cleave(cube1, xsplit[0], ysplit[0], zsplit[0])
-                cubes1 = set()
-                for c in tcubes1:
-                    cubes1 |= cleave(c, xsplit[1]+1, ysplit[1]+1, zsplit[1]+1)
+                # Cleave two cubes at the two opposite points where the cubes intersect
+                point1 = (xsplit[0], ysplit[0], zsplit[0])
+                point2 = (xsplit[1]+1, ysplit[1]+1, zsplit[1]+1)
 
-                tcubes2 = cleave(cube2, xsplit[0], ysplit[0], zsplit[0])
-                cubes2 = set()
-                for c in tcubes2:
-                    cubes2 |= cleave(c, xsplit[1]+1, ysplit[1]+1, zsplit[1]+1)
+                cubes1 = cleave_2points(cube1, point1, point2)
+                cubes2 = cleave_2points(cube2, point1, point2)
 
                 return (cubes1, cubes2)
 
@@ -262,33 +138,12 @@ def intersect(cube1, cube2):
 # If cubes don't intersect, return None
 # else return {cube1 - cube2}
 def remove_cube(cube1, cube2):
-    if inside(cube1, cube2):
-        return set()
-
     c1, c2 = intersect(cube1, cube2)
-    # print(f"  c1: {sum([cube_size(x) for x in c1])} {c1}")
-    # print(f"  c2: {sum([cube_size(x) for x in c2])} {c2}")
     if len(c1)==1 and len(c2)==1 and c1 != c2:
         # No overlap
         return None
 
     return c1 - c2
-
-
-def total_size(cubes):
-    total = 0
-    all = list(cubes)
-    for a,add in enumerate(all):
-        print(f"{a+1}. {add}")
-        # print (f"{a+1}. + {t} = {total} - {dupes} = {total-dupes}")
-        keep, cube = add
-        if not keep:
-            continue
-        univ = frozenset([cube])
-        for sub in all[a+1:]:
-            univ = remove_from_cuboids(univ, sub[1])
-        total += sum([cube_size(x) for x in univ])
-    return total #sum([cube_size(x) for x in cubes])
 
 def remove_from_cuboids(cuboids, cube):
     add = set()
@@ -307,38 +162,19 @@ def remove_from_cuboids(cuboids, cube):
     cuboids |= add
     return cuboids
 
-
 def part2(input):
-    cuboids = []
-
-    # prev = (0,0,0,0,0,0)
-
-    for row in input:
-        if not row:
+    total = 0
+    for a,add in enumerate(input):
+        keep, cube = add
+        print(f"{a+1}. {' on' if keep else 'off'} {cube}")
+        if not keep:
             continue
-        cmd,_,x0,x1,_,y0,y1,_,z0,z1 = row
-        x0=int(x0)
-        y0=int(y0)
-        z0=int(z0)
-        x1=int(x1)
-        y1=int(y1)
-        z1=int(z1)
-        x0,x1 = sorted([x0,x1])
-        y0,y1 = sorted([y0,y1])
-        z0,z1 = sorted([z0,z1])
-
-        cube = (x0,x1,y0,y1,z0,z1)
-
-        if cmd == 'on':
-            cuboids.append((True, cube))
-        else:
-            cuboids.append((False, cube))
-            # cuboids = set(remove_from_cuboids(frozenset(cuboids), cube))
-
-        print(row, f"Num cubes={len(cuboids)}")#"  Size = {universe(cuboids)}")#"   Total size={total_size(cuboids)}")
-
-    print(f"Size = {universe(cuboids)}")
-    return total_size(cuboids)
+        univ = frozenset([cube])
+        for sub in input[a+1:]:
+            univ = remove_from_cuboids(univ, sub[1])
+        size = sum([cube_size(x) for x in univ])
+        total += size
+    return total #sum([cube_size(x) for x in cubes])
 
 from aocd import data
-runAll(sample, data, parse, part1, part2, answer1, answer2)
+runAll('', data, parse, part1, part2, None, None)
