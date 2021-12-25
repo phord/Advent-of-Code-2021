52226798e8ab0397e8c75fbf1310c91548d343fe
Phil Hord <phord@purestorage.com>
2021-12-24 18:52:37 -0800

    Day 25:  And day 24

--- 24.py
+++ 24.py
@@ -1,22 +1,86 @@
 #!/bin/python3
 
 from core.skel import *
+import random
 
 sample = """
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 14
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 12
+mul y x
+add z y
 """
 answer1 = None
 answer2 = None
 
 def parse(input):
-    lines = [parse_fields(x,digits+alpha) for x in input.splitlines() if x]
+    lines = [x.split(' ') for x in input.splitlines() if x]
     return lines
 
-def part1(input, part2=False):
+def run(program, stdin):
+    reg = {"w": 0, "x": 0, "y": 0, "z": 0, }
+
+    prev = 0
+    print(f"==== INPUT: {''.join([x for x in stdin])} ====")
+    line = 1
+    for op in program:
+        var = op[1]
+        if op[0] == 'inp':
+            reg[var] = int(stdin[0])
+            stdin = stdin[1:]
+        else:
+            operand = reg[op[2]] if str(op[2]) in 'wxyz' else int(op[2])
+            if op[0] == 'add':
+                reg[var] += operand
+            elif op[0] == 'mul':
+                reg[var] *= operand
+            elif op[0] == 'div':
+                reg[var] //= operand
+            elif op[0] == 'mod':
+                reg[var] %= operand
+            elif op[0] == 'eql':
+                reg[var] = 1 if reg[var] == operand else 0
+
+        print(line, ' '.join([str(x) for x in op]), "   ", ' '.join([f"{var}={reg[var]}" for var in reg]))
+        line += 1
+    return reg['z']
+
+def part1(input):
+
+    ## 14 digits have a palindrom relationship (7 each)
+    ## 1  2  3  4  5  6  7  8  9 10 11 12 13 14
+    ##       ====
+    ##                   ====  d8 = d7-1
+    ##                         ====  d10 = d9+2
+    ##                =================  d11 = d6+3
+    ##             =======================  d12 = d5-5
+    ##             =======================  d13 = d2-7
+    ##             =======================  d14 = d1+6
+
+    #      12345678901234
+    stdin="39999698799429"
+    stdin="18116121134117"
+
+    result = run(input, stdin)
+    print (result, input)
     return None
 
 
 def part2(input):
-    return part1(input, True)
+    return None ## part1(input, True)
 
 
 from aocd import data
--- 25.py
+++ 25.py
@@ -3,16 +3,75 @@
 from core.skel import *
 
 sample = """
-"""
+v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>"""
 answer1 = None
 answer2 = None
 
 def parse(input):
-    lines = [parse_fields(x,digits+alpha) for x in input.splitlines() if x]
-    return lines
+    lines = [x for x in input.splitlines() if x]
+
+    south = set()
+    east = set()
+    for row,line in enumerate(lines):
+        for col,c in enumerate(line):
+            if c == 'v':
+                south.add((row,col))
+            elif c == '>':
+                east.add((row,col))
+
+    return (east, south, (len(lines), len(lines[0])))
+
+def move(orig, dir, pop, size):
+    x = (orig[0] + dir[0]) % size[0]
+    y = (orig[1] + dir[1]) % size[1]
+    pos = (x,y)
+    if pos in pop:
+        return orig
+    else:
+        return pos
+
+def step(east, south, size):
+    east_next = set()
+    south_next = set()
+    pop = east | south
+    for pos in east:
+        east_next.add(move(pos, (0,1), pop, size))
+
+    pop = east_next | south
+    for pos in south:
+        south_next.add(move(pos, (1,0), pop, size))
+
+    return (east_next, south_next)
+
+def dump(east, south, size):
+    height, width  = size
+    for row in range(height):
+        line = ['>' if (row,col) in east else 'v' if (row,col) in south else '.' for col in range(width)]
+        print(''.join(line))
+    print("---")
 
 def part1(input, part2=False):
-    return None
+    x = 0
+    east, south, size = input
+    while True:
+        x += 1
+        # dump(east, south, size)
+        # print(x)
+        east_next, south_next = step(east, south, size)
+        if not (east_next - east or south_next - south):
+            break
+        east = east_next
+        south = south_next
+
+    return x
 
 
 def part2(input):
